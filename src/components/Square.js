dab115b73ce539fb7b14402460e08363d4cb0ad0
Logan Wohlers <lawohlers@gmail.com>
2019-05-02 20:56:15 -0700

    added pieces file- working on getting them to show

--- src/components/Square.js
+++ src/components/Square.js
@@ -14,9 +14,9 @@ class Square extends React.Component {
     //     this.setState({ clicked: val })
     // }
     render() {
-        const color = this.state.clicked ? 'red' : 'lightgray'
+        const color = this.state.clicked ? 'red' : 'white'
         return (
-            <td id={this.props.data} >
+            <td >
                 <div className="square" style={{ backgroundColor: color }} >
                     {this.props.data}
                 </div>
--- src/containers/GameBoard.js
+++ src/containers/GameBoard.js
@@ -1,34 +1,56 @@
 import React from 'react'
 import Square from '../components/Square'
 
+const block = [
+    [1, 1],
+    [1, 1]
+]
+
 class GameBoard extends React.Component {
 
     constructor() {
         super()
         this.state = {
-            grid: Array(20).fill(Array(20).fill(0)),
+            grid: Array(20).fill(Array(10).fill(0)),
         }
     }
 
+    materialize(piece) {
+        let copyGrid = [...this.state.grid]
+        debugger
+        for (let i = 0; i < piece.length; i++) {
+            for (let j = 0; j < piece[i].length; j++) {
+                copyGrid[i][j] = piece[i][j]
+            }
+        }
+        this.setState({ grid: copyGrid })
+    }
+
+    componentDidMount() {
+        this.materialize(block)
+    }
+
     render() {
+
         const style = {
             textAlign: "center",
             tableLayout: 'fixed',
         };
+
+
 
         const gameGrid = this.state.grid.map((row, i) => {
             return (
                 <tr key={"row" + i}>
                     {row.map((col, j) => {
                         return (
-                            <Square key={"col" + j} />
+                            <Square key={i + ":" + j} data={this.state.grid[i][j]} />
                         )
                     }
                     )}
                 </tr>
             )
         })
-
         return (
             <table cellSpacing="0" id="table" style={style}>
                 <tbody>
