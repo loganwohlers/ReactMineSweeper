de86171bb8e2cb33d94b93d6523a5b729d73c406
Logan Wohlers <lawohlers@gmail.com>
2019-05-03 13:28:25 -0700

    hid values in squares

--- src/components/Square.js
+++ src/components/Square.js
@@ -2,22 +2,34 @@ import React from 'react'
 
 class Square extends React.Component {
 
-  constructor(props) {
-    super(props)
-    this.state = {
-      clicked: false
+    constructor(props) {
+        super(props)
+        this.state = {
+            clicked: false
+            //display hidden-- once clicked -> true
+        }
     }
-  }
 
-  render() {
-    return (
-      <td >
-        <div className="square" >
-          {this.props.data}
-        </div>
-      </td>
-    )
-  }
+    handleClick = (e) => {
+        if (this.props.data === 'b') {
+            alert("YOU LOSE")
+        }
+        let clicked = !this.state.clicked
+        this.setState({ clicked })
+    }
+
+    render() {
+        let cssClick = this.state.clicked ? 'clickedsquare' : 'unclickedsquare'
+        return (
+            <td >
+                <div className="square" onClick={this.handleClick}>
+                    <div className={cssClick}>
+                        {this.props.data}
+                    </div>
+                </div>
+            </td >
+        )
+    }
 }
 
 export default Square;
