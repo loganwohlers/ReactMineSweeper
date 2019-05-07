78373f695728d0ae2a2ef64b00b830c129776038
Logan Wohlers <lawohlers@gmail.com>
2019-05-07 15:34:09 -0700

    board knows timer on gameOver

--- src/components/Square.js
+++ src/components/Square.js
@@ -4,51 +4,51 @@ import mine from '../assets/mine.png'
 
 class Square extends React.Component {
 
-  handleClick = () => {
-    this.props.handleSquareClick(this.props.coords)
-  }
-
-  handleFlag = (e) => {
-    e.preventDefault()
-    if (!this.props.revealed) {
-      this.props.handleFlagClick(e, this.props.coords)
+    handleClick = () => {
+        this.props.handleSquareClick(this.props.coords)
     }
-  }
 
-  determineCSS() {
-    if (this.props.flagged) {
-      return 'flaggedSquare'
-    } else if (this.props.revealed || (this.props.dead && this.props.data === 'b')) {
-      return 'clickedSquare'
-    } else {
-      return 'unclickedSquare'
+    handleFlag = (e) => {
+        e.preventDefault()
+        if (!this.props.revealed) {
+            this.props.handleFlagClick(e, this.props.coords)
+        }
     }
-  }
 
-  determineContent() {
-    if (this.props.flagged) {
-      return <img className='flag' src={flag} alt='' />
-    } else if (this.props.dead && this.props.data === 'b') {
-      return <img className='mine' src={mine} alt='' />
-    } else {
-      return this.props.data
+    determineCSS() {
+        if (this.props.flagged) {
+            return 'flaggedSquare'
+        } else if (this.props.revealed || (this.props.gameOver && this.props.data === 'b')) {
+            return 'clickedSquare'
+        } else {
+            return 'unclickedSquare'
+        }
     }
-  }
 
-  render() {
-    return (
-      <td >
-          <div
-            className="square"
-            onClick={this.handleClick}
-            onContextMenu={this.handleFlag}>
-              <div className={this.determineCSS()}>
-                {this.determineContent()}
-              </div>
-          </div>
-      </td >
-    )
-  }
+    determineContent() {
+        if (this.props.flagged) {
+            return <img className='flag' src={flag} alt='' />
+        } else if (this.props.gameOver && this.props.data === 'b') {
+            return <img className='mine' src={mine} alt='' />
+        } else {
+            return this.props.data
+        }
+    }
+
+    render() {
+        return (
+            <td >
+                <div
+                    className="square"
+                    onClick={this.handleClick}
+                    onContextMenu={this.handleFlag}>
+                    <div className={this.determineCSS()}>
+                        {this.determineContent()}
+                    </div>
+                </div>
+            </td >
+        )
+    }
 }
 
 export default Square;
